8b63aa3ccc1e50f51dc7264a7d8c17f33d8eda1a
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2023-07-16 11:50:57 -0500

    Optional modifier

---
+++ src/intermediate/optional_modifier.ts
@@ -0,0 +1,16 @@
+type Person_1 = {
+  name: string;
+  email: string;
+  phone?: number;
+};
+
+const bruce: Person_1 = {
+  name: "Bruce",
+  email: "lakoqk@aks.com",
+};
+
+const alfred: Person_1 = {
+  name: "Alfred",
+  email: "wjewuej@iewi.com",
+  phone: 9283928,
+};
