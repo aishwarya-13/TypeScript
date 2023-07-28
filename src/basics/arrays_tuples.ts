00ed0ecb1f9316b7e7a80601f8f74a072b07496a
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2023-07-28 15:00:33 -0500

    arrays and tuples

--- src/basics/arrays_tuples.ts
+++ src/basics/arrays_tuples.ts
@@ -1,8 +1,14 @@
+//Array
 let array: number[] = [1, 2, 3, 4, 5];
+
+//usage
 array = [100, 2000, 3000];
+array = ["hello"]; //error
 
 //Tuple
 let tuple: [number, number] = [0, 0];
+
+//usage
 tuple = [1, 1];
 tuple = [2, 6];
 tuple = [5]; //Error - must be two items
