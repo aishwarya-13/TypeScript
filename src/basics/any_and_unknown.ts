1c7d85e4a4d81b7d7d0a0e1f348fbea6cb820b02
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2023-07-28 18:27:02 -0500

    Any and unknown

--- src/basics/any_and_unknown.ts
+++ src/basics/any_and_unknown.ts
@@ -1,3 +1,19 @@
+/**
+ * Any and unknown are super tyoes in TS
+ * This means all types of variables can be assigned to something that is any and same is true for unknown
+ *
+ * Any is like a escape hatch in type system. It gives you complete freedom to do anything you want with the variable
+ * It allows you to completely bypass the type system
+ * Any variable can be assigned to well-defined type
+ *
+ * Unknown is different
+ * You cannot access any properties on it and cannot assign to well-defined types
+ * If you want to use a unknown type variable as a string you have to check if it is a string
+ * If you want to use it as a boolean you have to check if it is a boolean
+ * So unknown is more type safe version of any
+ * It allows free access to all types within type system but also in safe manner
+ */
+
 let exampleAny: any;
 let exampleUnknown: unknown;
 
@@ -11,16 +27,18 @@ exampleUnknown = "Hi TS";
 
 //any
 exampleAny.allows.anything.you.can.imagine();
-let anysetBool: boolean = exampleAny;
+let anysetBool: boolean = exampleAny; //can assign to well-defined type
 
 //unknown
 exampleUnknown.trim();
 let unknownSetBoolean: boolean = exampleUnknown; //Error: Type 'unknown' is not assignable to type 'boolean'
 
+//If you want to use a unknown type variable as a string you have to check if it is a string
 if (typeof exampleUnknown == "string") {
   exampleUnknown.trim();
 }
 
+//If you want to use it as a boolean you have to check if it is a boolean
 if (typeof exampleUnknown === "boolean") {
 }
 
