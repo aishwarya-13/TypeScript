249e684b4d73be129c04e72d3d592e1ebe3b5d23
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2023-07-28 16:03:27 -0500

    functions

--- src/basics/functions.ts
+++ src/basics/functions.ts
@@ -1,21 +1,25 @@
 type Add = (a: number, b: number) => number;
 
-let add: Add;
+let addMe: Add;
+//let addMe: (a: number, b: number) => number;
 
-add = function (a: number, b: number): number {
+addMe = function (a: number, b: number): number {
   return a + b;
 };
-// function add(a: number, b: number): number {
-//   return a + b;
-// }
 
-function log(message: string): void {
+// takes input as string and returns a number
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+// takes input as string and does not return anything so return type is void
+function logMe(message: string): void {
   console.log(message);
 }
 
 function sum(...values: number[]) {
-  return values.reduce((previous, current) => {
-    return previous + current;
+  return values.reduce((prev, curr) => {
+    return prev + curr;
   });
 }
 
