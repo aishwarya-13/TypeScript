c6589a51a5ba5448307fb265b59e7b8a832f5b3b
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2023-07-29 18:54:47 -0500

    Class parameter properties

---
+++ src/intermediate/class_parameter_properties.ts
@@ -0,0 +1,22 @@
+/**
+ * This way it declares the properties and also initializes them
+ */
+class Person_4 {
+  constructor(public name: string, public age: number) {}
+}
+
+/**
+U see there is duplication between the declaration of the properties and the constructor parameters.
+TS allows us to get rid of this duplication using parameter properties.
+ */
+// class Person_4 {
+//   public name: string;
+//   public age: number;
+//   constructor(name: string, age: number) {
+//     this.name = name;
+//     this.age = age;
+//   }
+// }
+
+const adam = new Person_4("Adam", 120000);
+console.log(adam.name, adam.age);
