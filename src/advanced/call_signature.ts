5b0fc397ab9296b43245e5a5678b8d3e5a6f9a54
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2023-08-01 15:06:07 -0500

    call signature

--- src/advanced/call_signature.ts
+++ src/advanced/call_signature.ts
@@ -1,7 +1,10 @@
+//Below call sign is how normally a function type is defined
 //type Add_1 = (a: number, b: number) => number;
 
 /**
- * This sign is same as type Add_1 = (a: number, b: number) => number;
+ * Below call sign is same as this -> "type Add_1 = (a: number, b: number) => number"
+ * Other way of defining a call sign is as below in a body block and replace => with :
+ * You can also add other properties and also a function overload
  */
 // interface Add_1 {
 //   (a: number, b: number): number;
@@ -19,16 +22,21 @@ const add_1: Add_1 = (a: number, b: number, c?: number) => {
 
 add_1.debugName = "Addition function";
 
-/////////////////
+/**
+ * Class
+ */
+
+//type PointCreator = new (x: number, y: number)=> {x: number; y: number};
+
 type PointCreator = {
   new (x: number, y: number): {x: number; y: number};
 };
 
 // type PointCreator = {
 //   new (x: number, y: number): {x: number; y: number};
-//   new (x: number, y: number, z: number): {x: number; y: number; z: number};
+//   new (x: number, y: number, z: number): {x: number; y: number; z: number};//constructor overload
 //   (x: number, y: number): {x: number; y: number};
-//   (x: number, y: number, z: number): {x: number; y: number; z: number};
+//   (x: number, y: number, z: number): {x: number; y: number; z: number};//function overload
 //   debugName?: string;
 // };
 
